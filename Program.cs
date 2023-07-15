1aa40f56c41943cb02b82eb285473fb70d25f2a0
Sean Dorris <seanc.dorris@gmail.com>
2023-07-15 12:49:50 -0500

    testing pushes with vs and vs code

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
   new Plant()
   {
     Species = "Norway Maple",
-    LightNeeds = 3,
+    LightNeeds = 5,
     AskingPrice = 100.00M,
     City = "Nashville",
     Zip = 37221,
@@ -12,7 +12,7 @@
     new Plant()
   {
     Species = "Japanese Honeysuckle",
-    LightNeeds = 3,
+    LightNeeds = 4,
     AskingPrice = 150.00M,
     City = "Seattle",
     Zip = 98101,
@@ -30,7 +30,7 @@
     new Plant()
   {
     Species = "Hemlock",
-    LightNeeds = 3,
+    LightNeeds = 2,
     AskingPrice = 175.00M,
     City = "Denver",
     Zip = 80014,
@@ -39,7 +39,7 @@
     new Plant()
   {
     Species = "Scotch Broom",
-    LightNeeds = 3,
+    LightNeeds = 1,
     AskingPrice = 125.00M,
     City = "Portland",
     Zip = 97035,
@@ -59,7 +59,7 @@ string welcome = "Welcome to ExtraVert!";
 
 Console.WriteLine(welcome);
 string? choice = null;
-while (choice != "E")
+while (choice != "G")
 {
     Console.WriteLine(@"Choose an option:
                         A. Display All Plants
@@ -179,5 +179,22 @@ void PlantOfDay ()
 
 void FilterLightNeeds ()
 {
+  List<Plant> lightFilter = new List<Plant>();
 
+  Console.WriteLine("Please enter a maximum light need");
+
+  double maxLightInput = Convert.ToDouble(Console.ReadLine());
+
+  foreach (Plant plant in plants)
+  {
+    if (plant.LightNeeds <= maxLightInput)
+    {
+      lightFilter.Add(plant);
+    }
+  }
+
+  for (int i=0; i < lightFilter.Count; i++)
+  {
+    Console.WriteLine($"{i + 1}. {lightFilter[i].Species}");
+  }
 };
